b6ae8ab0716e8144d57a8c42b05a7dd2745c1186
Moxie Sozo <accounts@moxiesozo.com>
2016-10-06 12:53:13 -0600

    SB: Leader Board Service Updates

--- postar2/www/js/app-min.js
+++ postar2/www/js/app-min.js
@@ -143,15 +143,44 @@ angular.module( 'app.services')
 angular.module( 'app.services')
 .service( 'LeaderBoardService', ['$firebaseArray', function($firebaseArray){
 
-  var $service =  {
-    get_leaders : function(){
-    	var leadersRef = firebase.database().ref().child('postar_leaders');
-    	var postar_leaders = $firebaseArray(leadersRef);
-    }
-  }
+  var $service =  { }
+
 
 	$service.leadersRef = firebase.database().ref().child('postar_leaders');
   $service.leaders = $firebaseArray($service.leadersRef);
+
+
+  $service.leaders.$loaded().then(function(x) {
+    $service.getHighScore();
+  })
+
+  $service.leaders.$watch(function($a, $b){
+    $service.getHighScore();
+  })
+
+  $service.checkHighScore = function( score ){
+    console.log( score )
+    $service.getHighScore();
+    if(score > $service.leaders[0].score){
+      return true;
+    }
+  }
+
+  $service.getHighScore = function(){
+    $service.leaders.sort( function($a , $b ){
+      return $b.score - $a.score;
+    })
+    console.log( $service.leaders[0].score)
+  }
+
+
+  $service.saveScore = function( name , score){
+    $service.leaders.$add({ name: name , score : score }).then(function(ref) {
+      alert( 'Score Added')
+    });
+
+  }
+
 
   return $service
 }])
@@ -166,10 +195,12 @@ angular.module( 'app.services')
         {'year' : '2012', 'name' : 'Blah', 'src' : '2012.png'},
         {'year' : '2013', 'name' : 'Blah', 'src' : '2013.png'}
       ]
-    }
+    },
+
   }
 
   $service.posters = $service.get_posters();
+
 
   return $service;
 
